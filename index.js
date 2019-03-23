96a5af24fbb04b8dca6ddb8662f3f69981231f01
Thiago Cavalcante <thiagopcsantos@gmail.com>
2019-03-23 00:35:50 +0000

    index

---
+++ index.js
@@ -0,0 +1,18 @@
+const pronoun = ['the', 'our'];
+const adj = ['great', 'big'];
+const noun = ['jogger','racoon'];
+
+for (let a = 0; a < pronoun.length; a++) {
+
+  for (let b = 0; b < adj.length; b++) {
+    
+    for(let c = 0 ; c < noun.length; c++ ){
+      
+    
+    
+    console.log( pronoun[a] + " " + adj[b] +" "+ noun[c]);
+    
+    }
+  }
+}
+// JavaScript File
